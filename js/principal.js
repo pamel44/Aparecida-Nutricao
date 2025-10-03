6698f7ad7bb09a27f4f95efe04787ccbbd7113c3
Pamela Raiane <batista.paula.pamela@escola.pr.gov.br>
2025-10-03 09:45:39 -0300

    a

---
+++ js/principal.js
@@ -0,0 +1,5 @@
+       //console.log(document.querySelector("h1"));
+       var titulo = (document.querySelector("h1"));
+       console.log(titulo);
+       console.log(titulo.textContent);
+       titulo.textContent="Kauan Nutricions";
