6f7d391831bdb1892af939bafb319c9bd45c6f0a
Pamela Raiane <batista.paula.pamela@escola.pr.gov.br>
2025-10-03 11:02:57 -0300

    g

--- js/principal.js
+++ js/principal.js
@@ -3,3 +3,8 @@
        console.log(titulo);
        console.log(titulo.textContent);
        titulo.textContent="Pamela Nutricions";
+       
+       var paciente = document.querySelector("#primeiro-paciente");
+       console.log(paciente);
+       var tdPeso = paciente.querySelector(".info-peso");
+       console.log(tdPeso);
