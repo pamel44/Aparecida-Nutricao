a0a83dbed35d421a23d1876c2014d3393c1dbe32
Pamela Raiane <batista.paula.pamela@escola.pr.gov.br>
2025-10-21 09:46:23 -0300

    vvv

--- js/principal.js
+++ js/principal.js
@@ -2,29 +2,25 @@
 var titulo = (document.querySelector("h1"));
 console.log(titulo);
 console.log(titulo.textContent);
-titulo.textContent="Kauan Nutricions";
+titulo.textContent="Pamela Nutricions";
 
 
-var paciente = document.querySelector("#primeiro-paciente");
-var tdNome = document.querySelector(".info-nome");
-var nome = tdNome.textContent;
-console.log(nome)
+var pacientes = document.querySelectorAll(".paciente")
+for (var i = 0; i < pacientes.length ; i++){
 
 
-var tdPeso = document.querySelector(".info-peso");
+var paciente = pacientes [i];
+
+
+var tdPeso = paciente.querySelector(".info-peso");
 var peso = tdPeso.textContent;
-console.log(peso);
 
 
-var tdAltura = document.querySelector(".info-altura");     
+var tdAltura = paciente.querySelector(".info-altura");     
 var altura = tdAltura.textContent;
-console.log(altura);
 
 
 var tdImc = paciente.querySelector(".info-imc")
-var imc = peso/ (altura * altura);
-tdImc.textContent = imc;
-console.log(imc);
 //IMC = peso/altura * altura
 
 
@@ -32,14 +28,14 @@ var pesoEhValido = true;
 var alturaEhValido = true;
 
 
-if(peso < 0 || peso > 1000){
+if(peso <= 0 || peso >= 1000){
    console.log("Peso inválido");
    var pesoEhValido = false;
    tdImc.textContent = "Peso inválido";
 }
 
 
-if(altura < 0 || altura > 3.00){
+if(altura <= 0 || altura >= 3.00){
    console.log("Altura inválida!");
    var alturaEhValido = false;
    tdImc.textContent = "Altura inválida";
@@ -49,6 +45,7 @@ if(altura < 0 || altura > 3.00){
 if(alturaEhValido && pesoEhValido){
    var imc = peso / (altura*altura);
    tdImc.textContent = imc;
+}
 }
 
 
